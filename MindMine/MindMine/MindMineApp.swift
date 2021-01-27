4801ae33bd77d566bf89353f7eeb44dbe1b4453f
Hbarbaroux <hugo.barbaroux@free.fr>
2021-01-27 12:20:23 +0100

    Empty ios app

---
+++ MindMine/MindMine/MindMineApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindMineApp.swift
+//  MindMine
+//
+//  Created by Hugo on 27/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct MindMineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
