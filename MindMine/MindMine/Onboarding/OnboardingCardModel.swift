3ce21eb7fb1275dbf00e26d8f6b6e0f58c02cf27
Hbarbaroux <hugo.barbaroux@free.fr>
2021-07-14 21:49:54 +0100

    .DS_Store in gitignore

---
+++ MindMine/MindMine/Onboarding/OnboardingCardModel.swift
@@ -0,0 +1,8 @@
+//
+//  OnboardingCardModel.swift
+//  MindMine
+//
+//  Created by Hugo on 14/07/2021.
+//
+
+import Foundation
---
+++ MindMine/MindMine/Onboarding/OnboardingCardView.swift
@@ -0,0 +1,20 @@
+//
+//  OnboardingCardView.swift
+//  MindMine
+//
+//  Created by Hugo on 14/07/2021.
+//
+
+import SwiftUI
+
+struct OnboardingCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OnboardingCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingCardView()
+    }
+}
